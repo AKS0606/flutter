5afa5203d3cd6cae8fdc34403c7a33425d7588df
AKS0606 <sakochi987987@gmail.com>
2021-02-13 16:55:31 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//import 'package:english_words/english_words.dart';
+
+void main() =>  runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcom to Flutter',
+      home:Scaffold(
+        appBar:AppBar(
+          title: Text('Welcom to Flutter'),
+        ),
+        body: Center(
+          child:Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
