cf162b2392cc257818554e923cfc6cbf4e3fcd35
AKS0606 <sakochi987987@gmail.com>
2021-02-14 17:34:59 +0900

    use WordPair

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
@@ -7,14 +10,15 @@ void main() =>  runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcom to Flutter',
+      title: 'Welcome to Flutter',
       home:Scaffold(
         appBar:AppBar(
-          title: Text('Welcom to Flutter'),
+          title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child:Text('Hello World!'),
+          child:Text(wordPair.asPascalCase),
         ),
       ),
     );
